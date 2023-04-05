ad50caf10a3269e007cab544d513cdee57dd827f
zymzz1996 <71252200+zymzz1996@users.noreply.github.com>
2023-04-05 12:51:34 +0800

    init

---
+++ src/main/java/com/zym/EchoServerHandler.java
@@ -0,0 +1,24 @@
+package com.zym;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ src/main/java/com/zym/TimeEncoder.java
@@ -0,0 +1,23 @@
+package com.zym;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+public class TimeEncoder extends ChannelOutboundHandlerAdapter {
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+        UnixTime m = (UnixTime) msg;
+        ByteBuf encoded = ctx.alloc().buffer(4);
+        encoded.writeInt((int)m.value());
+        ctx.write(encoded, promise); // (1)
+    }
+
+//    public class TimeEncoder extends MessageToByteEncoder<UnixTime> {
+//        @Override
+//        protected void encode(ChannelHandlerContext ctx, UnixTime msg, ByteBuf out) {
+//            out.writeInt((int)msg.value());
+//        }
+//    }
+}
